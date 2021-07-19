1d9d427f725829e4d58edb6f98891931af7a9c16
tead1234 <tead1234@gmail.com>
2021-07-20 07:41:42 +0900

    html, css

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-// html 엘리멘트를 가져옴
-const hell1 = document.querySelector("div.title");
+const h1 = document.querySelector(".hello:first-child h1"); // :다음엔 그냥 이름을 넣어준거
 
-function handleTitleclick() {
-    hell1.style.color = "red";
-};
-function handleTitlemouse(){
-    hell1.innerText = "mouse is here!"
+function handleMouseEnter() {
+        h1.classList.toggle("active");
 }
-function handleTitlemouseleave(){
-    hell1.innerText = "mouse is gone!"
-}
-function handleresize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-    alert("copier!!")
-}
-function handleWindowOffline(){
-    alert("No Wifi");
-}
-function handleWindowOnline(){
-    alert("OMG WIFI")
-}
-//hell1.onclick("clik",handleTitleclick); // ()을 넣으면바로 실행인데 내가 하고싶은건 클릭후 실행
-hell1.addEventListener("mouseenter", handleTitlemouse); // ()을 넣으면바로 실행인데 내가 하고싶은건 클릭후 실행
-hell1.addEventListener("mouseleave", handleTitlemouseleave); // ()을 넣으면바로 실행인데 내가 하고싶은건 클릭후 실행
-
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOnline);
-fasa
+h1.addEventListener("click",handleMouseEnter);
